9ebd75fa49bf7d631057dbcf418b4b728fdd341b
asnif <asnifkl@gmail.com>
2025-11-19 11:07:11 +0530

    first commit

---
+++ app/components/nav.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  const isActive = (path: string) =>
+    pathname === path ? "text-blue-600 font-bold" : "text-white-700";
+
+  return (
+    <nav className="w-full p-4 shadow-sm bg-black flex justify-between items-center">
+      <h1 className="text-white-600 font-bold">MyApp</h1>
+
+      <div className="flex gap-6">
+        <Link href="/" className={isActive("/")}>
+          Home
+        </Link>
+
+        <Link href="/about" className={isActive("/about")}>
+          About
+        </Link>
+
+        <Link href="/users" className={isActive("/users")}>
+          Users
+        </Link>
+      </div>
+    </nav>
+  );
+}
