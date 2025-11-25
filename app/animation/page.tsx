69f6d6a9bd703f5617a9ba58118432c016da8998
asnif <asnifkl@gmail.com>
2025-11-25 13:23:02 +0530

    re

--- app/animation/page.tsx
+++ app/animation/page.tsx
@@ -11,14 +11,29 @@ export default function GsapUseEffectExample() {
   useEffect(() => {
     const tl = gsap.timeline({ defaults: { duration: 1 } });
 
-    tl.from(box.current, { opacity: 0, y: -40 })
-      .from(box1.current, { opacity: 0, y: 20 }, )
+    tl.from(box.current, { opacity: 1, y: -50 })
+      .from(box1.current, { opacity: 1, y: 50 }, )
      
   }, []);
 
   return (
     <div >
-     
+    <div
+        ref={box}
+        style={{
+          width: "120px",
+          height: "120px",
+          background: "blue",
+        }}
+      ></div>
+        <div
+        ref={box1}
+        style={{
+          width: "120px",
+          height: "120px",
+          background: "blue",
+        }}
+      ></div>
     </div>
   );
 }
